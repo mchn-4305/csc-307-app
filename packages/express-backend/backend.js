1abf8d1c43968a81111cf30e4fb9b71eecc82c2f
Micah Chen <micahchen34@gmail.com>
2024-10-03 23:10:47 -0700

    Added query functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,23 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
