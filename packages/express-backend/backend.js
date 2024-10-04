b8a1949a797dbcb0863d9eadd98708ab2146d5a2
Micah Chen <micahchen34@gmail.com>
2024-10-03 23:41:58 -0700

    adding all changes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,30 +46,40 @@ const findUserByName = (name) => {
     );
   };
   
-  app.get("/users", (req, res) => {
+app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
     } else {
-      res.send(users);
+        res.send(users);
     }
-  });
+    });
 
-  const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
-  
-  app.get("/users/:id", (req, res) => {
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
     let result = findUserById(id);
     if (result === undefined) {
-      res.status(404).send("Resource not found.");
+        res.status(404).send("Resource not found.");
     } else {
-      res.send(result);
+        res.send(result);
     }
-  });
+    });
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+    };
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+    });
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
